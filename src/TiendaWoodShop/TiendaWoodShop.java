092f9a1a28b3cea7e2b6496168a876886de35308
Manuel Fernández <149889281+macfer79@users.noreply.github.com>
2025-07-19 11:33:27 +0200

    Commit inicial

---
+++ src/TiendaWoodShop/TiendaWoodShop.java
@@ -0,0 +1,12 @@
+package TiendaWoodShop;
+
+public class TiendaWoodShop {
+
+    public static void main(String[] args) {
+        TiendaWoodShop prg = new TiendaWoodShop();
+        prg.inicio();
+    }
+    void inicio(){
+
+    }
+}
